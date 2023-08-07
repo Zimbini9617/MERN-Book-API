cf72a5b38fa8a15be393a992c50d3d70ccdfa8ee
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-07 14:41:15 +0200

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root';
+import ErrorPage from './error-page';
+import AllBooks from './pages/AllBooks';
+import AddBooks from './pages/AddBooks';
+import Home from './pages/Home';
+import UpdateBook from './pages/UpdateBook';
 
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/all-books",
+        element: <AllBooks />,
+      },
+      {
+        path: "/add-books",
+        element: <AddBooks />,
+      },
+      {
+        path: "/books/:id",
+        element: <UpdateBook />,
+      },
+    ],
+  },
+])
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='bg-gradient-to-r from-green-600 to-transparent text-white font-semibold flex items-center justify-between px-8 py-2'>
+      <h1 className='text-white font-mono font-bold'>BOOK STORE</h1>
+      <ul className='flex gap-5'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/all-books">AllBooks</Link></li>
+        <li><Link to="/add-books">AddBooks</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
